90dc0f157455c6c6b50918c8933703d779ea1177
Franze Jr <franzejr@gmail.com>
2016-08-26 00:06:56 -0300

    Make api calls using FetchAPI

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Network,
   View
 } from 'react-native';
 
 class apiCalls extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { movies: [] }
+  }
+
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.movies);
+        this.setState({movies: responseJson.movies});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  componentWillMount(){
+    this.getMoviesFromApiAsync();
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          API Calls
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        { this.state.movies.map((item, index)=>(
+          <Text key={index} style={styles.instructions}>
+            {item.title} - {item.releaseYear}
+          </Text>))
+        }
       </View>
     );
   }
